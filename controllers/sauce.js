f07598663053c92be0a2fe92323e48f947cc16e5
GriffonAnton <griffonanton@gmail.com>
2022-12-28 09:56:07 +0100

    first commit

---
+++ controllers/sauce.js
@@ -0,0 +1,60 @@
+const Sauce = require('../models/sauce');
+
+
+exports.getAllSauce = (req, res, next) => {
+    Sauce.find().then(
+        (sauces) => {
+            res.status(200).json(things);
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+};
+
+exports.createSauce = (req, res, next) => {
+
+};
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({
+        _id: req.params.id
+    }).then(
+        (sauce) => {
+            res.status(200).json(thing);
+        }
+    ).catch(
+        (error) => {
+            res.status(404).json({
+                error: error
+            });
+        }
+    );
+};
+
+exports.modifySauce = (req, res, next) => {
+
+};
+
+exports.deleteSauce = (req, res, next) => {
+    Sauce.deleteOne({ _id: req.params.id }).then(
+        () => {
+            res.status(200).json({
+                message: 'Deleted!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+};
+
+exports.likeSauce = (req, res, next) => {
+
+};
